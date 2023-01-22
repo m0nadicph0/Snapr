7f2b23304fe4f5fb827537cca9e5a3f293c5f07e
m0nadicph0 <m0nadic.ph0@gmail.com>
2023-01-22 14:21:55 +0530

    preparing the UI

--- app/src/main/java/com/example/snapr/MainActivity.kt
+++ app/src/main/java/com/example/snapr/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.snapr
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.btn_camera).setOnClickListener { onLaunchCamera() }
+    }
+
+    private fun onLaunchCamera() {
+        Toast
+            .makeText(this, "Launching Camera", Toast.LENGTH_LONG)
+            .show()
     }
 }
